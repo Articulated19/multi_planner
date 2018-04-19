211c3e69c4140ba410c95159bf32a9319772efb7
alebabu <aleksandar@aleks>
2018-04-19 19:51:35 +0200

    The problem with the beam search is that it can't get all the neighbors

--- path_finder/multi_planner.cpp
+++ path_finder/multi_planner.cpp
@@ -94,6 +94,8 @@ public:
     int pathsize = 0;
     while(beam.size()){
       //1.Remove the best node from the beam
+      //cout<<"Picked Node"<<endl;
+      //cout<<"("<<beam.top()->getPosition()->getX()<<","<<beam.top()->getPosition()->getY()<<") : "<<beam.top()->getCurrentFvalue()<<endl;
       current = beam.top();
       beam.pop();
       //2.If we find a goal then we backtrace back to the start and return path
@@ -101,28 +103,19 @@ public:
         return backtrace(startnode,current);
       //3.Get neighbors from current
       Point2D** neighbors = current->getNeighbours();
-
-      //TODO Fix this we have segmantation fault
+      //TODO I have no idea how to iterrate through an array of point2D.
       //4.Add each successor to beam and record it's parent
-		  /*for(int i = 0; i < sizeof(neighbors)/sizeof(neighbors[0]); i++){
-        Node* succ = getNode(*(neighbors+i));
-        succ->setParent(current);
-        succ->setCurrentFvalue(fvalue(current->getPosition(),*(neighbors+i),endpoint));
-        beam.push(succ);
-      }*/
-      //Point2D* curNeighbor = *neighbors;
-      while(*neighbors != NULL){
-        Node* succ = getNode(*neighbors);
-        succ->setParent(current);
-        succ->setCurrentFvalue(fvalue(current->getPosition(),*neighbors,endpoint));
-        beam.push(succ);
+      while(*neighbors){
+        cout<<(*neighbors)->getX()<<endl;
+        cout<<(*neighbors)->getY()<<endl;
         neighbors++;
       }
-      cout<<"Here I am!"<<endl;
-      //5.If size of beam > beamSize then remove the largest elements from beam.
+
       if(beam.size() > beamSize){
         priority_queue<Node*, vector<Node*>, NodeCompare> temp;
+        //cout<<"new beam"<<endl;
         for(int i = 0; i < beamSize; i++){
+          //cout<<"("<<beam.top()->getPosition()->getX()<<","<<beam.top()->getPosition()->getY()<<") : "<<beam.top()->getCurrentFvalue()<<endl;
           temp.push(beam.top());
           beam.pop();
         }
@@ -133,17 +126,23 @@ public:
 
   }
 
-  //TODO Implement an algorithm that backtrace from a node to their parrent.
   Point2D** backtrace(Node* startNode, Node* goalNode){
     int pathSize = goalNode->getTreeSize();
     Point2D* newPath[pathSize];
     pathSize--;
     Node* current = goalNode;
-    while(!goalNode->equals(startNode)){
-      newPath[pathSize--] = current->getPosition();
+    while(1){
+      path[pathSize] = current->getPosition();
+      pathSize--;
       current = current->getParent();
+      if(current->equals(startNode)){
+        path[pathSize] = current->getPosition();
+        //cout<<"("<<current->getPosition()->getX()<<","<<current->getPosition()->getY()<<")"<<endl;
+        break;
+      }
     }
-    return newPath;
+    cout<<"Done!"<<endl;
+    return path;
   }
 
   double fvalue(Point2D* current, Point2D* point, Point2D* goal){
